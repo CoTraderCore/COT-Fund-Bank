b194557fed55558202e0b85933899d2bbd88a6c7
RuslanMirov <36946243+RuslanMirov@users.noreply.github.com>
2018-12-18 04:00:32 -0500

    Update ISmartBank.sol

--- ISmartBank.sol
+++ ISmartBank.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.4.24;
-import "./zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract SmartBank {
+contract ISmartBank {
 
-  function _addToken(address _token) private {
+  function addTokenInBank(address _token) public {
 
   }
 
   function sendETHToFund(uint256 _value) public {
-  
+
   }
 
 }
